987d38edb23b19e4a5016b65ca0ccbba17af7f77
Deepak Khajuria <9539519+dk307@users.noreply.github.com>
2022-09-16 15:51:21 -0700

    Rename project to Lite (#53)
    Also Update Nuget Packages

--- plugin/Hspi/PlugInData.cs
+++ plugin/Hspi/PlugInData.cs
@@ -10,12 +10,12 @@ namespace Hspi
         /// <summary>
         /// The plugin Id
         /// </summary>
-        public const string PlugInId = @"HomeKitController";
+        public const string PlugInId = @"HomeKitControllerLite";
 
         /// <summary>
         /// The plugin name
         /// </summary>
-        public const string PlugInName = @"HomeKit Controller";
+        public const string PlugInName = @"HomeKit Controller Lite";
 
     }
 }
